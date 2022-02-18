1426a8a6f25f9bea51c71bcc4fcf30ff72f67da9
Anna Carolina Rodeiro Abreu <anna.rodeiro@neon.com.br>
2022-02-18 14:03:35 -0300

    Finaliza o JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,38 @@ function createCalculator() {
   return {
     display: document.querySelector('.display'),
 
+    start() {
+      this.clickButtons();
+      this.pressEnter();
+    },
+
+    pressEnter() {
+      this.display.addEventListener('keyup', (e) => {
+        if(e.keyCode === 13) {
+          this.calculate();
+        }
+      });
+    },
+
+    calculate() {
+      let calculation = this.display.value;
+
+      try {
+        calculation = eval(calculation);
+
+        if(!calculation) {
+          alert('Conta inválida!');
+          return;
+        }
+
+        this.display.value = calculation;
+
+      } catch (e) {
+        alert('Conta inválida!');
+        return;
+      }
+    },
+
     clearDisplay() {
       this.display.value = '';
     },
@@ -10,35 +42,28 @@ function createCalculator() {
       this.display.value = this.display.value.slice(0, -1);
     },
 
-    calculate() {
-      
-    },
-    
-    start() {
-      this.clickButtons();
-    },
-
     clickButtons() {
-      document.addEventListener('click', function(e) {
+
+      document.addEventListener('click', (e) => {
         const el = e.target;
 
-        if(el.classList.contais('btn-num')) {
+        if (el.classList.contains('btn-num')) {
           this.btnForDisplay(el.innerText);
         }
 
-        if(el.classList.contains('btn-clear')) {
+        if (el.classList.contains('btn-clear')) {
           this.clearDisplay();
         }
 
-        if(el.classList.contains('btn-del')) {
+        if (el.classList.contains('btn-del')) {
           this.deleteOne();
         }
 
-        if(el.classList.contains('btn-eq')) {
+        if (el.classList.contains('btn-eq')) {
           this.calculate();
         }
 
-      }.bind(this));
+      });
     },
 
     btnForDisplay(value) {
